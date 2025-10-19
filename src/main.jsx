396fb41dafc64bfb4a4ab38a1725b3e609140e22
Zakhar-T <zakhar.null@gmail.com>
2025-10-19 14:59:27 +0300

    set basic file structure

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import 'modern-normalize';
+import './styles/index.css';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './components/App';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
