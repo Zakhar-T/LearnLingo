d73d8f29f2d100c9c75b71be164b695cd91eabe7
Zakhar-T <zakhar.null@gmail.com>
2025-10-20 23:07:48 +0300

    set up app router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+
+const HomePage = lazy(() => import('../pages/HomePage'));
+const TeachersPage = lazy(() => import('../pages/TeachersPage'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
+
 export default function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/teachers" element={<TeachersPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header/Header';
+import Layout from '../components/Layout';
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <Layout></Layout>
+    </>
+  );
+}
